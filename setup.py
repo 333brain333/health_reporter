01b1cf101872d35284516ae487cf2a5b810d8234
Драницкий Игорь <i.dranitskiy@cognitivepilot.com>
2022-02-01 15:38:18 +0300

    added ci pipeline

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+import setuptools
+import subprocess
+
+
+def parse_requirements():
+    """Parse requirements.txt."""
+    here = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(here, 'requirements.txt')) as f:
+        lines = f.readlines()
+    lines = [
+        l
+        for l in map(lambda l: l.strip(), lines)
+        if l != '' and l[0] != '#'
+    ]
+    return lines
+
+requirements = parse_requirements()
+
+setuptools.setup(
+    name='cpilot-health-reporter',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    description="Health Reporter for CognitivePilot",
+    url=(
+        "https://gitlab.cognitivepilot.com/ar13/web-service-tool/agrobox"
+    ),
+    author="Igor Dranitskiy",
+    author_email="i.dranitskiy@cognitivepilot.com",
+    license="MIT",
+    install_requires=requirements,
+)
